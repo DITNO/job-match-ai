bc8e6cf4d61f38eb1002c110cf1ffd238bf4b7f5
ditno <workpriyanshu23@gmail.com>
2026-07-24 23:39:28 +0530

    cleaner.py

---
+++ src/job_match/cleaner.py
@@ -0,0 +1,8 @@
+from nltk.tokenize import word_tokenize
+
+def tokenize_text(text: str) -> list[str]:
+    """
+    Takes a raw string and breaks it down into individual tokens using NLTK.
+    """
+    # TODO: write your tokenization logic here
+    pass
