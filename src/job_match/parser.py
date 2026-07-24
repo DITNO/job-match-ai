83c2a4a235b8efa834f0cec40e4207b408542629
ditno <workpriyanshu23@gmail.com>
2026-07-24 23:22:25 +0530

    file path checker

---
+++ src/job_match/parser.py
@@ -0,0 +1,21 @@
+
+from pathlib import Path
+
+
+def _valid_path(path):
+    file_path = Path(path)
+    if not file_path.exists():
+        raise FileNotFoundError(f"File not found:{file_path}")
+    return file_path
+
+def parse_resume(file_path):
+    #validate path
+    file_path = _valid_path(file_path)
+
+    #open and read
+    with open(file_path,'r') as f:
+        text = f.read()
+
+    #return text
+    return text
+
