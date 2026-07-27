5629a97977b4692bac750557f064c20d1ee1f608
ditno <workpriyanshu23@gmail.com>
2026-07-27 23:57:40 +0530

    cleaner.py

---
+++ src/job_match/cleaner.py
@@ -0,0 +1,38 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import string
+
+nltk.download('punkt', quiet=True)
+nltk.download('stopwords', quiet=True)
+nltk.download('punkt_tab', quiet=True)
+
+def clean_text(text):
+    # lower case 
+
+    text = text.lower()
+
+    # tokenize
+    tokens = text.split()
+
+    #remove punctuation and stop words
+
+    stop_words = set(stop_words('english'))
+
+    filtered = []
+
+    for token in tokens:
+        if token not in stop_words and token not in string.punctuation:
+            filtered.append(token)
+
+
+    stemmer = PorterStemmer()
+
+    stemmed = []
+    for token in filtered:
+        stemmed.append(stemmer.stem(token))
+
+
+    #return cleaned tokens as a list
+    return token
